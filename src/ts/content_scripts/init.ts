4151dc7bcc61d7cec8e1aba67366c745bdf4569c
YoshiyukiKato <yo.shuuuuki@gmail.com>
2017-08-24 15:04:21 +0900

    update gimmickry version

--- src/ts/content_scripts/init.ts
+++ src/ts/content_scripts/init.ts
@@ -1,9 +1,9 @@
-import Gim from "../../../lib/gimmickry";
+import {App} from "gimmickry";
 
 if(!window.__importUser__ && !window.__importView__){
-  const app = new Gim.App();
+  const app = new App();
   if(window.__importUser__ && window.__importView__){
-    console.log("Dev app is added!");
+    console.log("Dev app is initialized");
   }else{
     console.log("There are something wrong...");
   }
--- src/ts/popup/index.tsx
+++ src/ts/popup/index.tsx
@@ -2,6 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 
-console.log(React, ReactDOM);
-
 ReactDOM.render(<App/>, document.querySelector("#main"));
